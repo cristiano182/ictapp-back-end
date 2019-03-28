b593c8db245bcf998a02dcb6292c75e01e76a2e7
cristiano <cristiano182@gmail.com>
2019-03-28 16:15:30 -0300

    delte

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Files = require("../models/Files");
+const jwt = require("jsonwebtoken");
+const JWT_KEY = "my_secret_key";
+const User = require("../models/User");
 
 //////////////////// GET ALL UCS
 router.get("/files/:skip", async (req, res) => {
@@ -28,20 +31,19 @@ router.put("/files/:id", async (req, res) => {
   }
 });
 /////////////////////////// DELETAR UMA INFO
-router.delete("/files", async (req, res) => {
+router.post("/files/delete", async (req, res) => {
   try {
     const { uc_id, info_id } = req.body;
-
     let token = req.headers["authorization"].split(" ")[1];
     const decoded = jwt.verify(token, JWT_KEY);
     const user = await User.findOne({ userID: decoded.userID });
     if (user) {
+      if(user.role === 1){
       let obj = await Files.findById(uc_id);
-
-      let newArray = await obj.filter(p => p._id !== info_id);
+      let newArray = await obj.info.filter(p => p._id != info_id);
       obj.info = newArray;
       await obj.save();
-
+      }
       res.sendStatus(200);
     } else {
       res.sendStatus(200);
