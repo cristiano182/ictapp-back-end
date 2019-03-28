a3b178651dd5cae2488d38707d9caab1c1a3cb92
cristiano <cristiano182@gmail.com>
2019-03-28 15:21:22 -0300

    delete

--- src/routes/files.js
+++ src/routes/files.js
@@ -27,7 +27,32 @@ router.put("/files/:id", async (req, res) => {
     res.send(err);
   }
 });
+/////////////////////////// DELETAR UMA INFO
+router.delete("/files", async (req, res) => {
+  try {
+    const { uc_id, info_id } = req.body;
+
+    let token = req.headers["authorization"].split(" ")[1];
+    const decoded = jwt.verify(token, JWT_KEY);
+    const user = await User.findOne({ userID: decoded.userID });
+    if (user) {
+      let obj = await Files.findById(uc_id);
+
+      let newArray = await obj.filter(p => p._id !== info_id);
+      obj.info = newArray;
+      await obj.save();
+
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(200);
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 ///////////////// ADD NOVA UC
+/*
 router.post("/files", async (req, res) => {
 try {
   const { name, info } = req.body;
@@ -37,12 +62,6 @@ try {
 } catch (err) {
   res.send(err)
 }
-});
-
-router.post("/teste", async (req, res) => {
-  const obj = req.body;
-  res.send(obj);
-  console.log(obj);
-});
+});*/
 
 module.exports = router;
