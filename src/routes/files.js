ed9b1707d9f3a20ce0c398dd6212236e436558d5
cristiano <cristiano182@gmail.com>
2019-04-03 00:54:35 -0300

    ok

--- src/routes/files.js
+++ src/routes/files.js
@@ -10,7 +10,7 @@ router.get("/files/:skip", async (req, res) => {
   try {
     let skip = parseInt(req.params.skip);
     const files = await Files.find()
-      .sort({ info: "desc" })
+      .sort({ name })
       .skip(skip)
       .limit(20);
     res.send(files);
